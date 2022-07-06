4702750a04c25360fc750795caaaf7a2c7052a06
Salman H <Salman0112358@outlook.com>
2022-07-06 20:59:43 +0100

    fixing shorteststring function

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -18,9 +18,8 @@ function findShortestString(stringArr) {
 }
 
 
-
-console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
 /*
+console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
 console.log(findShortestString(["why", "hello", "to", "you"]), "to");
 console.log(findShortestString(["brave", "dance"]), "brave");
 */
