6d54b713eecbf2203ce23d6ad6671ee0d0486cc5
Salman H <Salman0112358@outlook.com>
2022-07-06 20:47:32 +0100

    fixing jest test cases

--- src/find-shortest-string-test.js
+++ src/find-shortest-string-test.js
@@ -4,9 +4,9 @@ const findShortestString = require("./find-shortest-string");
 
 test ("findShortestString identifies the shortest string present in an array ", () =>{
     expect(findShortestString(["it", "is", "a", "nice", "day"])).toBe("a");
-    expect(findLongestString(["why", "hello", "to", "you"])).toBe("to");
-    expect(findLongestString(["cheese","wack","moose"])).toBe("wack");
-    expect(findLongestString(["salman","sal","alman","lman","man"])).toBe("sal");
+    expect(findShortestString(["why", "hello", "to", "you"])).toBe("to");
+    expect(findShortestString(["cheese","wack","moose"])).toBe("wack");
+    expect(findShortestString(["salman","sal","alman","lman","man"])).toBe("sal");
 
 })
 
